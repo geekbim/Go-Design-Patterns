50f8113467077529c327182137d42aa2689ed415
geekbim <manyuabim9@gmail.com>
2022-03-03 09:47:57 +0700

    pull

---
+++ structural_patterns/flyweight/dressFactory.go
@@ -0,0 +1,27 @@
+package main
+
+const (
+	TerroristDressType        = "tDress"
+	CounterTerroristDressType = "ctDress"
+)
+
+var (
+	dressFactorySingleInstance = &dressFactory{
+		dressMap: make(map[string]dress),
+	}
+)
+
+type dressFactory struct {
+	dressMap map[string]dress
+}
+
+func (d *dressFactory) getDressByType(dressType string) (dress, error) {
+	if d.dressMap[dressType] != nil {
+		return d.dressMap[dressType], nil
+	}
+
+	if dressType == TerroristDressType {
+		d.dressMap[dressType] = newCounterTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+}
